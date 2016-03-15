ca2325a8694d146663f847c5809792bccb560576
Patryk Miroslaw <miroslaw.patryk@gmail.com>
2016-03-15 14:02:19 +0100

    inital

---
+++ Mandatory1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mandatory1.Startup))]
+namespace Mandatory1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
